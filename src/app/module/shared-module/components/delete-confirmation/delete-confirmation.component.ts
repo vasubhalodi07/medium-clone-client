367409f8c8a3432c8ba9492b4113934c87b15253
vasubhalodi07 <vasubhalodi111@gmail.com>
2024-05-13 10:42:57 +0530

    delete-confirmation: removed

---
+++ src/app/module/shared-module/components/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,22 @@
+// import { Component, Input, Output } from '@angular/core';
+// import { EventEmitter } from 'stream';
+
+// @Component({
+//   selector: 'app-delete-confirmation',
+//   templateUrl: './delete-confirmation.component.html',
+//   styleUrl: './delete-confirmation.component.css',
+// })
+// export class DeleteConfirmationComponent {
+//   @Input() confirmationMessage: string = 'Are you sure you want to delete?';
+//   @Output() confirm = new EventEmitter<void>();
+//   @Output() cancel = new EventEmitter<void>();
+//   @Input() showConfirmation: boolean = false;
+
+//   confirmDelete() {
+//     this.confirm.emit();
+//   }
+
+//   cancelDelete() {
+//     this.cancel.emit();
+//   }
+// }
